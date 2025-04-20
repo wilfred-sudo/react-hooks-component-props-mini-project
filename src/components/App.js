d292adc9b0070fc4790c349631418ba5145da574
wilfredkiprop <kipropwilfred90@gmail.com>
2025-04-20 13:55:09 +0300

    props

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import blogData from "../data/blog";
+import About from "./About";
+import Article from "./Article";
+import ArticleList from "./ArticleList";
+import Header from "./Header";
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <About />
+      <Article />
+      <ArticleList />
+      <Header />
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+  },
+};
